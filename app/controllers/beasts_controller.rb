da032423df5843393f09ec004cdcf3cbefbf4c84
csaument <c.s.aument@gmail.com>
2023-04-03 15:34:39 -0700

    reverses http error handling for 404 and 422. adds conditional validation to #update and #destroy.

--- app/controllers/beasts_controller.rb
+++ app/controllers/beasts_controller.rb
@@ -1,14 +1,14 @@
 class BeastsController < ApplicationController
-      rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
-      rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+      # rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+      # rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-      def render_unprocessable_entity_response(exception)
-            render json: exception.record.errors, status: :unprocessable_entity
-      end
+      # def render_unprocessable_entity_response(exception)
+      #       render json: exception.record.errors, status: :unprocessable_entity
+      # end
         
-      def render_not_found_response(exception)
-            render json: { error: exception.message }, status: :not_found
-      end
+      # def render_not_found_response(exception)
+      #       render json: { error: exception.message }, status: :not_found
+      # end
 
       def index
             # Return all entries as an array of JSON objects
@@ -25,15 +25,21 @@ class BeastsController < ApplicationController
       def update
             # Update an existing entry
             beast = Beast.find(params[:id])
-            beast.update!(beast_params)
-            render json: beast
+            if beast.update(beast_params) 
+                  render json: beast
+            else
+                  render json: beast.errors
+            end
       end
 
       def destroy
             # Delete an existing entry
             beast = Beast.find(params[:id])
-            beast.destroy!
-            render json: beast
+            if beast.destroy
+                  render json: beast
+            else
+                  render json: beast.errors
+            end
       end
 
       # Private variables for strong parameters
