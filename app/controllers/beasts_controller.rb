ee56ddc171bd1bbc571badab154cadaab1799a4f
Manflo27 <maflores1987@yahoo.com>
2023-04-04 10:59:53 -0700

    adds request validation and model specs for completeness description length and error 422 handling post and update request

--- app/controllers/beasts_controller.rb
+++ app/controllers/beasts_controller.rb
@@ -1,15 +1,4 @@
 class BeastsController < ApplicationController
-      rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
-      rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-
-      def render_unprocessable_entity_response(exception)
-            render json: exception.record.errors, status: :unprocessable_entity
-      end
-        
-      def render_not_found_response(exception)
-            render json: { error: exception.message }, status: :not_found
-      end
-
       def index
             # Return all entries as an array of JSON objects
             beasts = Beast.all
@@ -30,15 +19,22 @@ class BeastsController < ApplicationController
       def update
             # Update an existing entry
             beast = Beast.find(params[:id])
-            beast.update!(beast_params)
-            render json: beast
+            beast.update(beast_params)
+            if beast.valid? 
+                  render json: beast
+            else
+                  render json: beast.errors, status: 422
+            end
       end
 
       def destroy
             # Delete an existing entry
             beast = Beast.find(params[:id])
-            beast.destroy!
-            render json: beast
+            if beast.destroy
+                  render json: beast
+            else
+                  render json: beast.errors
+            end
       end
 
       # Private variables for strong parameters
